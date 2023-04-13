f2d161ba6677cb04520b371b1a478cf1e614c2ef
Junhobu <68681792+Junhobu@users.noreply.github.com>
2023-04-13 13:53:02 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int out, in, person = 0, max = 0;
+
+	for (int i = 0; i < 4; i++) {
+		cin >> out >> in;
+
+		person += (in - out);
+
+		if (max < person)
+			max = person;
+	}
+	cout << max;
+}
