c3974273098feadba581d6389e05cf472cd8a577
Junhobu <68681792+Junhobu@users.noreply.github.com>
2024-04-29 03:14:07 +0900

    [Bronze V] Title: Serca, Time: 516 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,15 @@
+heart=[' @@@   @@@ ',
+       '@   @ @   @',
+       '@    @    @',
+       '@         @',
+       ' @       @ ',
+       '  @     @  ',
+       '   @   @   ',
+       '    @ @    ',
+       '     @     ']
+
+n=int(input())
+
+for i in range(n):
+    for _ in heart:
+        print(_)
