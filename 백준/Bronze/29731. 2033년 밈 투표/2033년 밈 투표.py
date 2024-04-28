88e82ba0c800f19c4a38d602ef09e94f85ae5977
Junhobu <68681792+Junhobu@users.noreply.github.com>
2024-04-28 16:23:37 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,22 @@
+meme=['Never gonna give you up',
+      'Never gonna let you down',
+      'Never gonna run around and desert you',
+      'Never gonna make you cry',
+      'Never gonna say goodbye',
+      'Never gonna tell a lie and hurt you',
+      'Never gonna stop']
+
+n=int(input())
+
+check=False
+
+for _ in range(n):
+    sentence=input()
+    if sentence not in meme:
+        check=True
+        break
+
+if check:
+    print("Yes")
+else:
+    print("No")
