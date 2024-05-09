f1cbb32e4380b7eeb7a3eee116038809ba04600d
Junhobu <68681792+Junhobu@users.noreply.github.com>
2024-05-09 13:23:00 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for i in range(15):
+    cat=input()
+    if 'w' in cat:
+        print('chunbae')
+        break
+    elif 'b' in cat:
+        print('nabi')
+        break
+    elif 'g' in cat:
+        print('yeongcheol')
+        break
