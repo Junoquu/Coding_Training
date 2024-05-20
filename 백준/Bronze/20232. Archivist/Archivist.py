2f9d7ff614491abccb86e88f83cf85ff1bb85faf
Junhobu <68681792+Junhobu@users.noreply.github.com>
2024-05-20 17:53:46 +0900

    [Bronze IV] Title: Archivist, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,8 @@
+years=["ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "SPbSU",
+       "ITMO", "ITMO", "ITMO", "ITMO", "ITMO", "PetrSU, ITMO",
+       "SPbSU", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU",
+       "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO"]
+
+year=int(input())
+
+print(years[year-1995])
