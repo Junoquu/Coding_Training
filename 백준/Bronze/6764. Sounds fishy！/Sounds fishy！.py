64a7b95c8955d6c8c829271ae04783b6e5e5e89a
Junhobu <68681792+Junhobu@users.noreply.github.com>
2024-05-25 17:53:54 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.py
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.py
@@ -1,40 +1,13 @@
-first=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
 
-up=False
-down=False
-same=False
-string=[]
-
-for _ in range(3):
-    n=int(input())
-    if first<n:
-        up=True
-    else:
-        up=False
-    if first>n:
-        down=True
-    else:
-        down=False
-    if first==n:
-        same=True
-    else:
-        same=False
-
-    if up:
-        string.append('Fish Rising')
-    elif down:
-        string.append('Fish Diving')
-    elif same:
-        string.append('Fish At Constant Depth')
-    first=n
-
-compare_string=[]
-
-for i in string:
-    if i not in compare_string:
-        compare_string.append(i)
-
-if len(compare_string)==1:
-    print(compare_string[0])
+if a<b<c<d:
+    print('Fish Rising')
+elif a>b>c>d:
+    print('Fish Diving')
+elif a==b==c==d:
+    print('Fish At Constant Depth')
 else:
     print('No Fish')
