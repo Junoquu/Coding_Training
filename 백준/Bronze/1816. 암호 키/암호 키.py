a3c67bfc2a81b197bae28d1cb9493815280786da
Junoquu <68681792+Junoquu@users.noreply.github.com>
2024-11-03 20:56:09 +0900

    [Bronze I] Title: 암호 키, Time: 1196 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    num = int(input())
+    
+    for i in range(2, 1000001):
+        if num % i == 0:
+            print("NO")
+            break
+        if i == 1000000:
+            print("YES")
