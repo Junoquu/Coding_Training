cb6d3d382381048de57d234d51da75ad8da134fd
Junoquu <68681792+Junoquu@users.noreply.github.com>
2024-11-17 11:24:21 +0900

    [Silver III] Title: N과 M (3), Time: 1712 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+def recur(num):
+    if num==M:
+        print(*ans)
+        return
+    for i in range(1, N+1):
+        ans.append(i)
+        recur(num+1)
+        ans.pop()
+    
+N,M = map(int,input().split())
+ans = []
+
+recur(0)
